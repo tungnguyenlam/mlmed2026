a9cbc523879e5868a6072d0b8e50db0bfe3261ab
tungnguyen <nguyenlamtung2005@gmail.com>
2026-02-04 01:54:09 +0700

    Create base unet model from scratch

---
+++ practice2/model.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+class UNet(nn.Module):
+    def __init__(self, n_channels=1, n_classes=1):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+
+        # Encode / Downscale
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(64, 128)
+        )
+        self.down2 = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(128, 256)
+        )
+        self.down3 = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(256, 512)
+        )
+        self.down4 = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(512, 1024)
+        )
+
+        # Decode / Upscale
+        self.up1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.conv1 = DoubleConv(1024, 512) # 512 (up) + 512 (skip) = 1024
+        self.up2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.conv2 = DoubleConv(512, 256) # 256 (up) + 256 (skip) = 512
+        self.up3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.conv3 = DoubleConv(256, 128) # 128 (up) + 128 (skip) = 256
+        self.up4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.conv4 = DoubleConv(128, 64)  # 64 (up) + 64 (skip) = 128
+
+        # Output
+        self.outc = nn.Conv2d(64, n_classes, kernel_size=1)
+
+    def forward(self, x):
+        # Encode
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+
+        # Decode
+        x = self.up1(x5)
+        x = torch.cat([x, x4], dim=1) # skip connect
+        x = self.conv1(x)
+        x = self.up2(x)
+        x = torch.cat([x, x3], dim=1)
+        x = self.conv2(x)
+        x = self.up3(x)
+        x = torch.cat([x, x2], dim=1)
+        x = self.conv3(x)
+        x = self.up4(x)
+        x = torch.cat([x, x1], dim=1)
+        x = self.conv4(x)
+        logits = self.outc(x)
+        return logits
+
+# Loss func
+
+class BCEDiceLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(BCEDiceLoss, self).__init__()
+        self.bce = nn.BCEWithLogitsLoss()
+
+    def forward(self, inputs, targets, smooth=1):
+        bce_loss = self.bce(inputs, targets)
+
+        inputs = torch.sigmoid(inputs)
+
+        inputs = inputs.view(-1)
+
+        targets = targets.view(-1)
+
+        intersection = (input * targets).sum()
+        dice_score = (2. * intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
+        dice_loss = 1 - dice_score
+        
+        return bce_loss + dice_loss
